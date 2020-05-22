02c08f6f5fae470d539a38682d1510ca50476da4
reiishikawa1208 <48386370+reiishikawa1208@users.noreply.github.com>
2020-05-22 20:01:40 +0900

    chnaging from username to first_name

--- allauth/socialaccount/providers/line/provider.py
+++ allauth/socialaccount/providers/line/provider.py
@@ -24,10 +24,7 @@ class LineProvider(OAuth2Provider):
 
     def extract_common_fields(self, data):
         return dict(email=data.get('email'),
-                    username=data.get('displayName'),
-                    first_name=data.get('first_name'),
-                    last_name=data.get('last_name'),
-                    name=data.get('name'))
+                    first_name=data.get('displayName'))
 
 
 provider_classes = [LineProvider]
